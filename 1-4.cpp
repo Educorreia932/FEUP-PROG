fd56a29de312a57e9ce3a8c35e3a2d9e1c66d65b
Eduardo Correia <skelozard1@gmail.com>
2019-02-18 19:00:31 +0000

    Exercise 1.4 added

---
+++ 1-4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a;
+	double b;
+	double c;
+	double d;
+	double e;
+	double f;
+
+	cout << "Insira os valores de a, b, c, d, e e f: ";
+	cin >> a, b, c, d, e, f;
+
+	double x = (c*e - b*f) / (a*e - b*d);
+	double y = (a*f - c*d) / (a*e - b*d);
+	
+	cout << "Os valores de x e de y são, respetivamente, " << x << "e " << y;
+
+	return 0;
+}
