38cd6a080b97fed2b1f3bb49afa7d2f38c97aaa4
Eduardo Correia <skelozard1@gmail.com>
2019-02-19 11:16:14 +0000

    Corrections

--- Práticas/Práticas/1-3.cpp
+++ Práticas/Práticas/1-3.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-	double M;
-	double p;
-	double r;
-	double pi = 3.14159;
+	float p;
+	float r;
+	float pi = 3.14159;
 
-	cout << "Introduza o valor de p (em Kg/m3): "; //"³" desconfigura
+	cout << "Introduza o valor de p (em Kg/m3): "; 
 	cin >> p;
 	cout << "Introduza o valor de r (em m): ";
 	cin >> r;
-	cout << "O valor de M (em Kg) e " << (4 / 3) * (p * pi * r * r * r * 3); //Acento desconfigura
+	cout << "O valor de M (em Kg) e " << (4 * (pi * p * pow(r, 3))) / 3;
 
 	return 0;
 }
