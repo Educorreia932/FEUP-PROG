008201eb068c36b5dcaddab28a52d6b296cb3b49
Eduardo Correia <skelozard1@gmail.com>
2019-02-22 09:08:04 +0000

    Lecture 4 created

---
+++ Teóricas/Teóricas/4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	bool failed;
+
+	do {
+		failed = false;
+		cout << "x ? ";
+		cin >> x;
+			
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(1000, '\n');
+			failed = true;
+		}
+	} while (failed);
+
+	cout << "x = " << x << endl;
+
+	return 0;
+}
