f5c3b58cb7e88322df4ec42458a754a7085b80dd
Eduardo Correia <skelozard1@gmail.com>
2019-03-07 10:49:48 +0000

    Resolved #1

--- Práticas/Práticas/2-13.cpp
+++ Práticas/Práticas/2-13.cpp
@@ -3,16 +3,23 @@
 using namespace std;
 
 int main() {
-	string result;
-	int number, divisor;
+	int number, divisor = 2;
 
 	cout << "Insert a number: ";
 	cin >> number;
+	cout << number << "=";
 
-	while (number > 1) {
+	do {
+		if (number % divisor == 0) {
+			number /= divisor;
+			cout << divisor << "x";
+		}
 
-		number = number / divisor
-	}
+		else
+			divisor += 1;
+	} while (number != divisor);
 
+	cout << number;
+	
 	return 0;
 }
